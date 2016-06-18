18372f5740e1bcb25e400cc541cbf537a07294ad
Dongwen Wang <dongwen.wang@gmail.com>
2016-06-18 17:50:21 -0400

    continuing on setting pages

--- Float/webapp/urls.py
+++ Float/webapp/urls.py
@@ -3,4 +3,4 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^search_result$', views.search_result, name='search_result'),]
+	url(r'^search_result/$', views.search_result, name='search_result'),]
--- Float/webapp/views.py
+++ Float/webapp/views.py
@@ -3,10 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import loader
 
 def index(request):
-	template = loader.get_template('home.html')
-	return HttpReponse(template.render(request))
+	return render(request, 'home.html')
 def search_result(request):
-	template = loader.get_template('search_results.html')
-	return HttpReponse(template.render(request))
+	return render(request, 'search_result.html')
+
 
 # Create your views here.
